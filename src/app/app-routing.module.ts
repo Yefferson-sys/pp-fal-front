13f322753662064ddd11b90457b0fb29f2324b91
Yefferson-sys <jefferson.caleno@uptc.edu.co>
2021-02-19 09:40:07 -0500

    Create and customize edit appointment page and appointment info shared component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './Components/auth/auth.component';
 import { AppointmentListComponent } from './Pages/appointment-list/appointment-list.component';
 import { AssignAppointmentComponent } from './Pages/assign-appointment/assign-appointment.component';
+import { EditAppointmentComponent } from './Pages/edit-appointment/edit-appointment.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { UserProfileComponent } from './Pages/user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppointmentListComponent
+    component: EditAppointmentComponent
   }
 ];
 
